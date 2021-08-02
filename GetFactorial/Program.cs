1af3c72a0d4f09edce71f012dbc04249f4d05394
殊怀丶 <lvzhihe_123@qq.com>
2021-08-02 15:32:32 +0800

    added GetFactorial

---
+++ GetFactorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace GetFactorial
+{
+    class Program
+    {
+        static double GetFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * GetFactorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一个数字：");
+            int number;
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("输入错误。");
+                return;
+            }
+            Console.WriteLine("{0}的阶乘是：{1}。", number, GetFactorial(number));
+        }
+    }
+}
