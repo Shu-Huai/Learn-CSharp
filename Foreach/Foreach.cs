4570b2276dff26d0242ab94d8f2b94ea89fc9dd3
殊怀丶 <lvzhihe_123@qq.com>
2021-08-02 21:50:50 +0800

    added Foreach

---
+++ Foreach/Foreach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Foreach
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach(int number in numbers)
+            {
+                Console.Write(number);
+                Console.Write(" ");
+            }
+        }
+    }
+}
