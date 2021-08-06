d98cebb04c8df10bddb5d7cc8076ea751bfc7223
殊怀丶 <lvzhihe_123@qq.com>
2021-08-06 22:22:14 +0800

    added Test Class

---
+++ Test Class/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Test_Class
+{
+    public class Person
+    {
+        private int age_;
+        private char gender_;
+        public string Name { get; set; }
+        public int Age
+        {
+            get => age_;
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                age_ = value;
+            }
+        }
+        public char Gender
+        {
+            get => gender_;
+            set
+            {
+                if (value != '男' && value != '女')
+                {
+                    value = '\0';
+                }
+                gender_ = value;
+            }
+        }
+        public Person(string name = "", int age = 0, char gender = '\0')
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+        public static void SayHello()
+        {
+            Console.WriteLine("你好。");
+        }
+        public override string ToString()
+        {
+            return $"{Name}，{Age}，{Gender}。";
+        }
+    }
+}
---
+++ Test Class/Test Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Test_Class
+{
+    class TestClass
+    {
+        static void Main()
+        {
+            Person person = new("吕陟赫", 18, '男');
+            Console.WriteLine(person);
+            Person.SayHello();
+        }
+    }
+}
