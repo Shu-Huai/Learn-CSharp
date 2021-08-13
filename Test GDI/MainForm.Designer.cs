fd5dec96ccf800efa03c990a4673315cf3294b58
殊怀丶 <lvzhihe_123@qq.com>
2021-08-14 00:14:13 +0800

    added my learn

---
+++ Test GDI/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Verification_Code
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.verificationCode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.verificationCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // verificationCode
+            // 
+            this.verificationCode.BackColor = System.Drawing.SystemColors.Control;
+            this.verificationCode.Location = new System.Drawing.Point(12, 12);
+            this.verificationCode.Name = "verificationCode";
+            this.verificationCode.Size = new System.Drawing.Size(150, 60);
+            this.verificationCode.TabIndex = 0;
+            this.verificationCode.TabStop = false;
+            this.verificationCode.Click += new System.EventHandler(this.ChangeVerificationCode);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 502);
+            this.Controls.Add(this.verificationCode);
+            this.Name = "MainForm";
+            this.Text = "验证码";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Repaint);
+            ((System.ComponentModel.ISupportInitialize)(this.verificationCode)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox verificationCode;
+    }
+}
+
---
+++ Test GDI/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Verification_Code
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Repaint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void ChangeVerificationCode(object sender, EventArgs e)
+        {
+            Random randomGetter = new();
+            string result = "";
+            Bitmap bmp = new(150, 60);
+            Graphics graphics = Graphics.FromImage(bmp);
+            verificationCode.Image = bmp;
+            for (int i = 0; i < 5; i++)
+            {
+                result += randomGetter.Next(0, 10);
+                string[] fonts = { "微软雅黑", "宋体", "黑体", "隶书", "Times New Roman", "仿宋", "楷体", "幼圆" };
+                Color[] colors = { Color.Yellow, Color.Red, Color.Black, Color.Orange, Color.Blue, Color.Green, Color.AliceBlue, Color.Azure };
+                graphics.DrawString(randomGetter.Next(0, 10).ToString(),
+                    new Font(fonts[randomGetter.Next(0, fonts.Length)], 40, FontStyle.Bold), new SolidBrush(colors[randomGetter.Next(0, colors.Length)]),
+                    new Point(i * 25, 0));
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                graphics.DrawLine(new Pen(Brushes.DarkBlue, 2),
+                    new Point(randomGetter.Next(0, bmp.Width), randomGetter.Next(0, bmp.Height)), new Point(randomGetter.Next(0, bmp.Width), randomGetter.Next(0, bmp.Height)));
+            }
+            for (int i=0;i<50;i++)
+            {
+                bmp.SetPixel(randomGetter.Next(0, bmp.Width), randomGetter.Next(0, bmp.Height), Color.Black);
+            }
+        }
+    }
+}
