33d075e911bd029ec2929a382a3e8c40ecf6608c
殊怀丶 <lvzhihe_123@qq.com>
2021-09-05 23:11:15 +0800

    added my learn

---
+++ Test Delegate/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Test_Delegate
+{
+    class Program
+    {
+        public delegate string Del(string str);
+        public static void DoStr(string[] list, Del del)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = del(list[i]);
+            }
+        }
+        public static string DoUpper(string str)
+        {
+            return str.ToUpper();
+        }
+        public static string DoLower(string str)
+        {
+            return str.ToLower();
+        }
+        public static string AddSimbol(string str)
+        {
+            return "\"" + str + "\"";
+        }
+        private static void Main()
+        {
+            string[] test = new string[] { "aaaa", "AAAAAAAA", "asD", "dd", "D" };
+            DoStr(test, new Del(DoUpper));
+            DoStr(test, delegate (string str)
+            {
+                return str.ToLower();
+            });
+            DoStr(test, (string str) => { return str.ToUpper(); });
+            return;
+        }
+    }
+}
